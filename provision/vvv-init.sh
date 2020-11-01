3c3d9a8202e772b96bfd5f7846ae16e4e3962576
davidwolfpaw <david@davidwolfpaw.com>
2020-11-01 09:28:53 -0500

    updates provision code

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -187,16 +187,16 @@ install_wp() {
 
   INITIAL_BASE_SETUP=$(get_config_value 'initial_base_setup' "")
   if [ ! -z "${INITIAL_BASE_SETUP}" ]; then
-    # install the themes and plugins that we do want
-    # noroot wp theme install https://orangeblossommedia.com/obm/tools/genesis.3.3.3.zip --activate
-    # noroot wp plugin install genesis-simple-edits
-    # noroot wp plugin install https://orangeblossommedia.com/obm/tools/advanced-custom-fields-pro.zip --activate
-    # noroot wp plugin install https://orangeblossommedia.com/obm/tools/gravityforms_2.4.21.3.zip --activate
-    # # delete sample post
-    # noroot wp post delete "$(noroot wp post list --post_type=post --posts_per_page=1 --post_status=publish --postname="hello-world" --field=ID --format=ids)" --force
-    # # delete sample page, and create homepage
-    # noroot wp post delete "$(noroot wp post list --post_type=page --posts_per_page=1 --post_status=publish --pagename="sample-page" --field=ID --format=ids)" --force
-    # # Add a comma separated list of pages
+    install the themes and plugins that we do want
+    noroot wp theme install https://orangeblossommedia.com/obm/tools/genesis.3.3.3.zip --activate
+    noroot wp plugin install genesis-simple-edits
+    noroot wp plugin install https://orangeblossommedia.com/obm/tools/advanced-custom-fields-pro.zip --activate
+    noroot wp plugin install https://orangeblossommedia.com/obm/tools/gravityforms_2.4.21.3.zip --activate
+    # delete sample post
+    noroot wp post delete "$(noroot wp post list --post_type=post --posts_per_page=1 --post_status=publish --postname="hello-world" --field=ID --format=ids)" --force
+    # delete sample page, and create homepage
+    noroot wp post delete "$(noroot wp post list --post_type=page --posts_per_page=1 --post_status=publish --pagename="sample-page" --field=ID --format=ids)" --force
+    # Add a comma separated list of pages
     allpages="Home,About,Contact,Blog"
     # create all of the pages
     IFS=","
@@ -218,19 +218,19 @@ install_wp() {
     do
       noroot wp menu item add-post main-navigation "$pageid"
     done
-    # Assign navigation to primary location
-    noroot wp menu location assign main-navigation primary
-    # Remove default widgets from sidebars
-    widgetsheaderright=$(noroot wp widget list header-right --format=ids)
-    noroot wp widget delete $widgetsheaderright
-    widgetsprimary=$(noroot wp widget list sidebar --format=ids)
-    noroot wp widget delete $widgetsprimary
-    widgetssecondary=$(noroot wp widget list sidebar-alt --format=ids)
-    noroot wp widget delete $widgetssecondary
-    # Create a category called "News" and set it as default
-    noroot wp term create category News
-    noroot wp option update default_category "$(noroot wp term list category --name=news --field=id)"
-    # update and add general options
+    # # Assign navigation to primary location
+    # noroot wp menu location assign main-navigation primary
+    # # Remove default widgets from sidebars
+    # widgetsheaderright=$(noroot wp widget list header-right --format=ids)
+    # noroot wp widget delete $widgetsheaderright
+    # widgetsprimary=$(noroot wp widget list sidebar --format=ids)
+    # noroot wp widget delete $widgetsprimary
+    # widgetssecondary=$(noroot wp widget list sidebar-alt --format=ids)
+    # noroot wp widget delete $widgetssecondary
+    # # Create a category called "News" and set it as default
+    # noroot wp term create category News
+    # noroot wp option update default_category "$(noroot wp term list category --name=news --field=id)"
+    # # update and add general options
     noroot wp option update date_format 'j F Y'
     noroot wp option update links_updated_date_format 'F j, Y g:i a'
     noroot wp option update timezone_string 'America/New_York'
